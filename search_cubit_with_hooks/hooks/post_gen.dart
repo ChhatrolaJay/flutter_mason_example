1323551f5d7d268d9c477d67cd0ff5e8ebcfea28
jayc <jayc@meditab.com>
2024-03-08 12:48:19 +0530

    -- flutter mason example added documentation.

--- search_cubit_with_hooks/hooks/post_gen.dart
+++ search_cubit_with_hooks/hooks/post_gen.dart
@@ -5,17 +5,12 @@ import 'package:mason/mason.dart';
 Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Installing packages');
 
-  final name = context.vars['name'];
-
-  // Use the `Logger` instance.
-  context.logger.info('Hello $name!');
-
   final path = context.vars['path'];
 
   // Use the `Logger` instance.
-  context.logger.info('Path $path!');
+  context.logger.info('Adding packages to Path- $path!');
 
-  // Run `flutter packages get` after generation.
+  // This will add the flutter_bloc to pubspec file.
   await Process.run(path, ['flutter', 'pub', 'add', 'flutter_bloc']);
   await Process.run(path, [
     'flutter',
